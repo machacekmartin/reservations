4202e2cd1c7777135ed5693483a7c8180f6fcf1d
Martin <macamp34@gmail.com>
2023-08-30 01:25:26 +0200

    Add base tables + reservations migrations

---
+++ database/migrations/2023_08_29_203938_create_reservations_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->timestamp('start_at');
+            $table->timestamp('end_at');
+            $table->timestamp('remind_at')->nullable();
+            $table->timestamp('canceled_at')->nullable();
+
+            $table->unsignedInteger('guest_count');
+            $table->boolean('fulfilled')->default(false);
+            $table->string('note')->nullable();
+
+            $table->foreignId('user_id')->constrained();
+        });
+    }
+};
---
+++ database/migrations/2023_08_29_205537_create_table_reservation_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('table_reservation', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->foreignId('table_id')->constrained();
+            $table->foreignId('reservation_id')->constrained();
+        });
+    }
+};
