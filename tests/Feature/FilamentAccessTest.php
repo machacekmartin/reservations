8d00920b56ed08abc933915e981e45a036e14a95
Martin <macamp34@gmail.com>
2023-08-31 01:40:18 +0200

    Add tests for accessing filament panel

---
+++ tests/Feature/FilamentAccessTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+
+it('allows access to filament admin panel to user with admin role', function () {
+    Role::create(['name' => 'admin']);
+    $user = User::factory()->as('admin')->create();
+
+    $this->actingAs($user)
+        ->get('/admin')
+        ->assertOk();
+});
+
+it('does not allow access to filament admin panel to user with role other than admin', function (string $role) {
+    Role::create(['name' => $role]);
+    $user = User::factory()->as($role)->create();
+
+    $this->actingAs($user)
+        ->get('/admin')
+        ->assertForbidden();
+})->with([
+    ['role' => 'manager'],
+    ['role' => 'user'],
+    ['role' => 'customer'],
+    ['role' => 'some-other-role']
+]);
+
+it('does not allow access to filament admin panel to user with no role', function () {
+    $user = User::factory()->create();
+
+    $this->actingAs($user)
+        ->get('/admin')
+        ->assertForbidden();
+});
