d045a49b9e93abc9c8ae8cefb1b68ceb7807cc8e
machacekmartin <machacekmartin@users.noreply.github.com>
2023-08-31 01:54:05 +0000

    Dusting

--- app/Filament/Resources/TableResource.php
+++ app/Filament/Resources/TableResource.php
@@ -112,7 +112,7 @@ class TableResource extends Resource
     {
         return [
             (string) __('Capacity') => (string) $record->capacity,
-            (string) __('Occuiped') => $record->isOccupied() ? __('Yes') : __('No')
+            (string) __('Occuiped') => $record->isOccupied() ? __('Yes') : __('No'),
         ];
     }
 }
