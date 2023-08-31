02eb2f3aebdd0bf5b7efd99b68449b85eabbbefd
Martin <macamp34@gmail.com>
2023-08-31 03:19:57 +0200

    Add base resources for table and reservation

---
+++ app/Filament/Resources/ReservationResource.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ReservationResource\Pages;
+use App\Filament\Resources\ReservationResource\RelationManagers;
+use App\Models\Reservation;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ReservationResource extends Resource
+{
+    protected static ?string $model = Reservation::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-calendar-days';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('user_id')
+                    ->relationship('user', 'name')
+                    ->required(),
+                Forms\Components\DateTimePicker::make('start_at')
+                    ->required(),
+                Forms\Components\DateTimePicker::make('end_at')
+                    ->required(),
+                Forms\Components\DateTimePicker::make('remind_at'),
+                Forms\Components\DateTimePicker::make('canceled_at'),
+                Forms\Components\TextInput::make('guest_count')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\Toggle::make('fulfilled')
+                    ->required(),
+                Forms\Components\TextInput::make('note')
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('user.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('start_at')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('end_at')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('remind_at')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('canceled_at')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('guest_count')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\IconColumn::make('fulfilled')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('note')
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListReservations::route('/'),
+            'create' => Pages\CreateReservation::route('/create'),
+            'edit' => Pages\EditReservation::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getNavigationGroup(): ?string
+    {
+        return __('Restaurant management');
+    }
+}
---
+++ app/Filament/Resources/TableResource.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TableResource\Pages;
+use App\Filament\Resources\TableResource\RelationManagers;
+use App\Models\Table as TableModel;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TableResource extends Resource
+{
+    protected static ?string $model = TableModel::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('label')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Toggle::make('available')
+                    ->inline(false)
+                    ->required(),
+                Forms\Components\TextInput::make('capacity')
+                    ->required()
+                    ->numeric(),
+                // Forms\Components\TextInput::make('location')
+                //     ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('label')
+                    ->searchable(),
+                Tables\Columns\IconColumn::make('available')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('capacity')
+                    ->numeric()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTables::route('/'),
+            'create' => Pages\CreateTable::route('/create'),
+            'edit' => Pages\EditTable::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getNavigationGroup(): ?string
+    {
+        return __('Restaurant management');
+    }
+}
