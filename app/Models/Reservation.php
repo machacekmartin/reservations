b3fbc6612eb8678be94851b4b4fa423d9e37c891
Martin <macamp34@gmail.com>
2023-09-01 00:01:11 +0200

    Apply BelongsToRestaurant trait

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\BelongsToRestaurant;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -12,7 +13,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
  */
 class Reservation extends Model
 {
-    use HasFactory;
+    use HasFactory, BelongsToRestaurant;
 
     protected $fillable = [
         'start_at',
--- app/Models/Table.php
+++ app/Models/Table.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\BelongsToRestaurant;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -9,7 +10,7 @@ use Illuminate\Support\Carbon;
 
 class Table extends Model
 {
-    use HasFactory;
+    use HasFactory, BelongsToRestaurant;
 
     protected $fillable = [
         'label',
