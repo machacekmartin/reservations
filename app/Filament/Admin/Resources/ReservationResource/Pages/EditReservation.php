7971ed0395f037db9da52c77e7ad15c4aa4e8f33
Martin <macamp34@gmail.com>
2023-09-01 19:46:10 +0200

    Add two dummy actions to edit page of reservation

--- app/Filament/Admin/Resources/ReservationResource/Pages/EditReservation.php
+++ app/Filament/Admin/Resources/ReservationResource/Pages/EditReservation.php
@@ -2,8 +2,13 @@
 
 namespace App\Filament\Admin\Resources\ReservationResource\Pages;
 
+use App\Enums\ReservationStatus;
 use App\Filament\Admin\Resources\ReservationResource;
+use App\Filament\Admin\Resources\ReservationResource\Widgets\ReservationOverview;
+use App\Models\Reservation;
 use Filament\Actions;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditReservation extends EditRecord
@@ -13,7 +18,51 @@ class EditReservation extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Actions\Action::make('fulfill')
+                ->disabled(fn () => ! in_array($this->record->status, [ReservationStatus::PENDING, ReservationStatus::LATE]))
+                ->icon(ReservationStatus::FULFILLED->getIcon())
+                ->color(ReservationStatus::FULFILLED->getColor())
+                ->modalWidth('max-w-xl')
+                ->modalHeading('Guests have arrived')
+                ->modalAlignment('center')
+                ->modalIcon(ReservationStatus::FULFILLED->getIcon())
+                ->modalDescription('To mark this reservation as fulfilled, please enter the time the guests have arrived.')
+                ->form([
+                    DateTimePicker::make('arrived_at')
+                        ->required()
+                        ->seconds(false)
+                        ->default(now()),
+                ])
+                ->action(function (array $data): void {
+                    $this->record->arrived_at = $data['arrived_at'];
+                    $this->record->status = ReservationStatus::FULFILLED;
+                    $this->record->save();
+                    Notification::make()->success()->title('Reservation fulfilled')->send();
+                }),
+
+            Actions\Action::make('cancel')
+                ->action(null)
+                ->disabled(fn () => ! in_array($this->record->status, [ReservationStatus::PENDING, ReservationStatus::LATE]))
+                ->icon(ReservationStatus::CANCELED->getIcon())
+                ->color(ReservationStatus::CANCELED->getColor())
+                ->modalWidth('max-w-xl')
+                ->modalHeading('Cancel this reservation')
+                ->modalAlignment('center')
+                ->modalIcon(ReservationStatus::FULFILLED->getIcon())
+                ->modalDescription('To mark this reservation as fulfilled, please enter the time of cancelation.')
+                ->form([
+                    DateTimePicker::make('canceled_at')
+                        ->required()
+                        ->seconds(false)
+                        ->default(now()),
+                ]),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            ReservationOverview::class,
         ];
     }
 }
