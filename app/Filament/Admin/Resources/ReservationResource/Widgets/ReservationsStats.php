6fdf8421e90ea2e9480a75ae247f8073935ef352
Martin <macamp34@gmail.com>
2023-09-01 19:45:31 +0200

    Rework existing reservations stats widget to accompany status column

--- app/Filament/Admin/Resources/ReservationResource/Widgets/ReservationsStats.php
+++ app/Filament/Admin/Resources/ReservationResource/Widgets/ReservationsStats.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Admin\Resources\ReservationResource\Widgets;
 
+use App\Enums\ReservationStatus;
 use App\Models\Reservation;
 use Filament\Support\Colors\Color;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
@@ -14,39 +15,58 @@ class ReservationsStats extends BaseWidget
     {
         return [
             // where start is only today
-            Stat::make('Todays pending reservations', $this->pendingReservations()->where('start_at', '>=', now()->startOfDay())->where('end_at', '<=', now()->endOfDay())->count())
-                ->description('Overall pending: ' . $this->pendingReservations()->count())
-                ->icon('heroicon-o-clock')
-                ->color(Color::Gray),
+            Stat::make('Todays pending / late',
+                $this->pending()->where('start_at', '>=', now()->startOfDay())->where('end_at', '<=', now()->endOfDay())->count() . ' / ' .
+                $this->late()->where('start_at', '>=', now()->startOfDay())->where('end_at', '<=', now()->endOfDay())->count()
+            )
+                ->description('Overall ' . $this->pending()->count() . ' pending and ' . $this->late()->count() . ' late')
+                ->icon(ReservationStatus::PENDING->getIcon())
+                ->color(ReservationStatus::PENDING->getColor()),
 
-            Stat::make('Todays fulfilled reservations', $this->fulfilledReservations()->where('start_at', '>=', now()->startOfDay())->where('end_at', '<=', now()->endOfDay())->count())
-                ->description('Overall fulfilled: ' . $this->fulfilledReservations()->count())
-                ->icon('heroicon-o-check-circle')
-                ->color(Color::Green),
+            Stat::make('Todays fulfilled ', $this->fulfilled()->where('start_at', '>=', now()->startOfDay())->where('end_at', '<=', now()->endOfDay())->count())
+                ->description('Overall fulfilled: ' . $this->fulfilled()->count())
+                ->icon(ReservationStatus::FULFILLED->getIcon())
+                ->color(ReservationStatus::FULFILLED->getColor()),
 
-            Stat::make('Todays canceled reservations', $this->canceledReservations()->where('start_at', '>=', now()->startOfDay())->where('end_at', '<=', now()->endOfDay())->count())
-                ->description('Overall canceled: ' . $this->canceledReservations()->count())
-                ->icon('heroicon-o-x-circle')
-                ->color(Color::Rose),
+            Stat::make('Todays canceled ', $this->canceled()->where('start_at', '>=', now()->startOfDay())->where('end_at', '<=', now()->endOfDay())->count())
+                ->description('Overall canceled: ' . $this->canceled()->count())
+                ->icon(ReservationStatus::CANCELED->getIcon())
+                ->color(ReservationStatus::CANCELED->getColor()),
+
+            Stat::make('Todays missed ', $this->missed()->where('start_at', '>=', now()->startOfDay())->where('end_at', '<=', now()->endOfDay())->count())
+                ->description('Overall missed: ' . $this->missed()->count())
+                ->icon(ReservationStatus::LATE->getIcon())
+                ->color(ReservationStatus::LATE->getColor()),
         ];
     }
 
-    private function pendingReservations(): Builder
+    protected function getColumns(): int
     {
-        return Reservation::query()
-            ->whereNull('canceled_at')
-            ->whereNull('fulfilled');
+        return 4;
     }
 
-    private function fulfilledReservations(): Builder
+    private function pending(): Builder
     {
-        return Reservation::query()
-            ->where('fulfilled', true);
+        return Reservation::query()->where('status', ReservationStatus::PENDING);
     }
 
-    private function canceledReservations(): Builder
+    private function late(): Builder
     {
-        return Reservation::query()
-            ->whereNotNull('canceled_at');
+        return Reservation::query()->where('status', ReservationStatus::LATE);
+    }
+
+    private function fulfilled(): Builder
+    {
+        return Reservation::query()->where('status', ReservationStatus::FULFILLED);
+    }
+
+    private function canceled(): Builder
+    {
+        return Reservation::query()->where('status', ReservationStatus::CANCELED);
+    }
+
+    private function missed(): Builder
+    {
+        return Reservation::query()->where('status', ReservationStatus::MISSED);
     }
 }
