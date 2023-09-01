ede48a1483d60bdcc3e3f877fd73a7b5d6358f95
Martin <macamp34@gmail.com>
2023-09-01 19:44:59 +0200

    Add base reservation overview widget

---
+++ app/Filament/Admin/Resources/ReservationResource/Widgets/ReservationOverview.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Filament\Admin\Resources\ReservationResource\Widgets;
+
+use App\Models\Reservation;
+use Filament\Support\Colors\Color;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Facades\Blade;
+
+class ReservationOverview extends BaseWidget
+{
+    public ?Reservation $record = null;
+
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Reservation length', $this->record->start_at->diffInMinutes($this->record->end_at) . ' minutes')
+                ->description('Starts at ' . $this->record->start_at->format('H:i') . ' and ends at ' . $this->record->end_at->format('H:i')),
+
+            Stat::make('Owner of reservation', $this->record->user->name)
+                ->description($this->record->user->email . ' | ' . $this->record->user->phone),
+
+            Stat::make('Status', $this->record->status->getLabel())
+                ->icon($this->record->status->getIcon())
+                ->color($this->record->status->getColor()),
+        ];
+    }
+
+    protected function getColumns(): int
+    {
+        return 3;
+    }
+}
