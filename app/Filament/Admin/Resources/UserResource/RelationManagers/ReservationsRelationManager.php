7482b78d791c5bc1981545477e686ef874462c57
Martin <macamp34@gmail.com>
2023-09-01 15:09:43 +0200

    Add relation manager in user resource to reservations

---
+++ app/Filament/Admin/Resources/UserResource/RelationManagers/ReservationsRelationManager.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Admin\Resources\UserResource\RelationManagers;
+
+use App\Filament\Admin\Resources\ReservationResource;
+use App\Models\Reservation;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Columns\Column;
+use Filament\Tables\Table;
+
+class ReservationsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'reservations';
+
+    public function table(Table $table): Table
+    {
+        $table = ReservationResource::table($table);
+
+        $table->getAction('edit')
+            ->action(fn (Reservation $record) => redirect()->to(ReservationResource::getUrl('edit', ['record' => $record])));
+
+        return $table;
+    }
+}
