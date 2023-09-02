fcd5e3405f0e991fb0886462fd5c660631819c2d
Martin <macamp34@gmail.com>
2023-09-03 01:40:14 +0200

    Add SendReservationReminders to scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Jobs\MarkLateReservations;
+use App\Jobs\SendReservationReminders;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,6 +15,10 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->job(new MarkLateReservations)
+            ->everyMinute()
+            ->withoutOverlapping();
+
+        $schedule->job(new SendReservationReminders)
             ->everyMinute()
             ->withoutOverlapping();
     }
--- tests/Feature/ScheduleTest.php
+++ tests/Feature/ScheduleTest.php
@@ -1,12 +1,21 @@
 <?php
 
 use App\Jobs\MarkLateReservations;
+use App\Jobs\SendReservationReminders;
 use Illuminate\Console\Scheduling\Schedule;
 
 it('schedules marking of pending reservations in the past as late', function () {
     $added = collect(app()->make(Schedule::class)->events())
         ->pluck('description')
         ->contains(MarkLateReservations::class);
+
+    expect($added)->toBeTrue();
+});
+
+it('schedules reservation reminders sending', function () {
+    $added = collect(app()->make(Schedule::class)->events())
+        ->pluck('description')
+        ->contains(SendReservationReminders::class);
 
     expect($added)->toBeTrue();
 });
