5f044f06a5249ec8759bd88b3b6d0c3760ddb5cc
machacekmartin <machacekmartin@users.noreply.github.com>
2023-09-02 12:53:20 +0000

    Dusting

--- app/Filament/Admin/Resources/RestaurantResource.php
+++ app/Filament/Admin/Resources/RestaurantResource.php
@@ -3,15 +3,12 @@
 namespace App\Filament\Admin\Resources;
 
 use App\Filament\Admin\Resources\RestaurantResource\Pages;
-use App\Filament\Admin\Resources\RestaurantResource\RelationManagers;
 use App\Models\Restaurant;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class RestaurantResource extends Resource
 {
--- app/Filament/Admin/Resources/RestaurantResource/Pages/CreateRestaurant.php
+++ app/Filament/Admin/Resources/RestaurantResource/Pages/CreateRestaurant.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Admin\Resources\RestaurantResource\Pages;
 
 use App\Filament\Admin\Resources\RestaurantResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateRestaurant extends CreateRecord
