6adc3d8247fc4fc1dcd05e2255f0e2068c795e42
Martin <macamp34@gmail.com>
2023-09-02 16:23:42 +0200

    Add methods to ReservationBuilder for scoping status

--- app/Builders/ReservationBuilder.php
+++ app/Builders/ReservationBuilder.php
@@ -2,9 +2,33 @@
 
 namespace App\Builders;
 
+use App\Enums\ReservationStatus;
 use Illuminate\Database\Eloquent\Builder;
 
 class ReservationBuilder extends Builder
 {
+    public function pending()
+    {
+        return $this->where('status', ReservationStatus::PENDING);
+    }
 
+    public function late()
+    {
+        return $this->where('status', ReservationStatus::LATE);
+    }
+
+    public function canceled()
+    {
+        return $this->where('status', ReservationStatus::CANCELED);
+    }
+
+    public function fulfilled()
+    {
+        return $this->where('status', ReservationStatus::FULFILLED);
+    }
+
+    public function missed()
+    {
+        return $this->where('status', ReservationStatus::MISSED);
+    }
 }
