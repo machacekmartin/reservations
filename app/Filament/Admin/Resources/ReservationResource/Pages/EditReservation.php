322b876ad26246adf5ad4d8efd52aa3e85e95c99
Martin <macamp34@gmail.com>
2023-09-02 22:07:47 +0200

    Update modal actions in edit reservation page

--- app/Filament/Admin/Resources/ReservationResource/Pages/EditReservation.php
+++ app/Filament/Admin/Resources/ReservationResource/Pages/EditReservation.php
@@ -18,15 +18,18 @@ class EditReservation extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\Action::make('fulfill')
+            Actions\Action::make('fulfill_reservation')
+                ->label('Arrived')
                 ->disabled(fn (Reservation $record) => ! in_array($record->status, [ReservationStatus::PENDING, ReservationStatus::LATE]))
                 ->icon(ReservationStatus::FULFILLED->getIcon())
                 ->color(ReservationStatus::FULFILLED->getColor())
                 ->modalWidth('max-w-xl')
-                ->modalHeading('Guests have arrived')
                 ->modalAlignment('center')
+                ->modalFooterActionsAlignment('center')
+                ->modalHeading('Guests have arrived')
                 ->modalIcon(ReservationStatus::FULFILLED->getIcon())
                 ->modalDescription('To mark this reservation as fulfilled, please enter the time the guests have arrived.')
+                ->modalSubmitActionLabel('Confirm')
                 ->form([
                     DateTimePicker::make('arrived_at')
                         ->required()
@@ -40,22 +43,43 @@ class EditReservation extends EditRecord
                     Notification::make()->success()->title('Reservation fulfilled')->send();
                 }),
 
-            Actions\Action::make('cancel')
-                ->action(null)
+            Actions\Action::make('miss_reservation')
+                ->label('Did not arrive')
+                ->disabled(fn (Reservation $record) => ! in_array($record->status, [ReservationStatus::PENDING, ReservationStatus::LATE]))
+                ->icon(ReservationStatus::MISSED->getIcon())
+                ->color(ReservationStatus::MISSED->getColor())
+                ->modalWidth('max-w-xl')
+                ->modalAlignment('center')
+                ->modalFooterActionsAlignment('center')
+                ->modalHeading('Guests did not arrive')
+                ->modalIcon(ReservationStatus::MISSED->getIcon())
+                ->modalDescription('Are you sure the guests did not arrive?')
+                ->modalSubmitActionLabel('Confirm')
+                ->action(function (Reservation $record): void {
+                    $record->status = ReservationStatus::MISSED;
+                    $record->save();
+                    Notification::make()->success()->title('Reservation marked as missed')->send();
+                }),
+
+            Actions\Action::make('cancel_reservation')
+                ->label('Cancel')
                 ->disabled(fn (Reservation $record) => ! in_array($record->status, [ReservationStatus::PENDING, ReservationStatus::LATE]))
                 ->icon(ReservationStatus::CANCELED->getIcon())
                 ->color(ReservationStatus::CANCELED->getColor())
                 ->modalWidth('max-w-xl')
-                ->modalHeading('Cancel this reservation')
                 ->modalAlignment('center')
-                ->modalIcon(ReservationStatus::FULFILLED->getIcon())
-                ->modalDescription('To mark this reservation as fulfilled, please enter the time of cancelation.')
-                ->form([
-                    DateTimePicker::make('canceled_at')
-                        ->required()
-                        ->seconds(false)
-                        ->default(now()),
-                ]),
+                ->modalFooterActionsAlignment('center')
+                ->modalHeading('Cancel this reservation')
+                ->modalIcon(ReservationStatus::CANCELED->getIcon())
+                ->modalDescription('Are you sure you want to cancel this reservation?')
+                ->modalSubmitActionLabel('Yes, cancel reservation')
+                ->modalCancelActionLabel('No')
+                ->action(function (Reservation $record): void {
+                    $record->canceled_at = now();
+                    $record->status = ReservationStatus::CANCELED;
+                    $record->save();
+                    Notification::make()->success()->title('Reservation canceled')->send();
+                }),
         ];
     }
 
