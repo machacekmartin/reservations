5de859ad9816326d8d63606787095a014571b304
Martin <macamp34@gmail.com>
2023-09-03 00:36:11 +0200

    Add dummy reservation reminder email with base view

---
+++ app/Mail/RemindReservation.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Reservation;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class RemindReservation extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public function __construct(Reservation $reservation)
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            from: new Address('notifications@reservations.test'),
+            subject: 'Remind Reservation',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.reservation-reminder',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
