9577744933a76fdad0f736dc1883282d2e740a80
Martin <macamp34@gmail.com>
2023-09-03 23:31:33 +0200

    Fix missing return type

--- app/Http/Controllers/LogoutUserController.php
+++ app/Http/Controllers/LogoutUserController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class LogoutUserController extends Controller
 {
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): RedirectResponse
     {
         auth()->logout();
 
