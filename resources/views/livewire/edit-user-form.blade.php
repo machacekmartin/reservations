3123c16e2c67285c04c8e8a516a9a0344ee8b5a0
Martin <macamp34@gmail.com>
2023-09-04 18:33:39 +0200

    Add missing edit-user-form view

---
+++ resources/views/livewire/edit-user-form.blade.php
@@ -0,0 +1,6 @@
+<div class="p-8 bg-white rounded-lg shadow"">
+    <h2 class="pb-4 mb-4 font-semibold border-b">Edit account information</h2>
+    <form action="submit">
+        {{ $this->form }}
+    </form>
+</div>
