c9836ce9237ecfbf11a60bc4460d0ddac3523b85
Martin <macamp34@gmail.com>
2023-09-05 22:02:44 +0200

    Fix table factory

--- database/factories/TableFactory.php
+++ database/factories/TableFactory.php
@@ -12,11 +12,15 @@ class TableFactory extends Factory
     public function definition(): array
     {
         return [
-            'label' => $this->faker->word(),
+            'label' => $this->faker->randomLetter(). $this->faker->randomLetter(),
             'available' => $this->faker->boolean(80),
             'capacity' => $this->faker->numberBetween(1, 10),
-            'location' => [],
-            'size' => [],
+            'dimensions' => [
+                'width' => $this->faker->numberBetween(50, 150),
+                'height' => $this->faker->numberBetween(50, 150),
+                'x' => $this->faker->numberBetween(1, 2000),
+                'y' => $this->faker->numberBetween(1, 1000),
+            ],
         ];
     }
 }
