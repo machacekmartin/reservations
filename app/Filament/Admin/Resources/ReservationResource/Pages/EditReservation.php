15143ddc362b784dad145797e90b6f8780c1a28e
Martin <macamp34@gmail.com>
2023-09-05 16:03:37 +0200

    Strip edit page of actions

--- app/Filament/Admin/Resources/ReservationResource/Pages/EditReservation.php
+++ app/Filament/Admin/Resources/ReservationResource/Pages/EditReservation.php
@@ -3,29 +3,9 @@
 namespace App\Filament\Admin\Resources\ReservationResource\Pages;
 
 use App\Filament\Admin\Resources\ReservationResource;
-use App\Filament\Admin\Resources\ReservationResource\Actions\CancelAction;
-use App\Filament\Admin\Resources\ReservationResource\Actions\FulfillAction;
-use App\Filament\Admin\Resources\ReservationResource\Actions\MissAction;
-use App\Filament\Admin\Resources\ReservationResource\Widgets\ReservationOverview;
 use Filament\Resources\Pages\EditRecord;
 
 class EditReservation extends EditRecord
 {
     protected static string $resource = ReservationResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            FulfillAction::make('fulfill_reservation'),
-            MissAction::make('miss_reservation'),
-            CancelAction::make('cancel_reservation'),
-        ];
-    }
-
-    protected function getHeaderWidgets(): array
-    {
-        return [
-            ReservationOverview::class,
-        ];
-    }
 }
