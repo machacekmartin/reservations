78181c88eba484092cfffca9adb52e5677b1a01c
Martin <macamp34@gmail.com>
2023-09-05 22:05:28 +0200

    Add filament theme to support classes outside of filament

--- resources/css/filament/admin/tailwind.config.js
+++ resources/css/filament/admin/tailwind.config.js
@@ -1,9 +1,9 @@
-import preset from './vendor/filament/support/tailwind.config.preset'
+import preset from '../../../../vendor/filament/filament/tailwind.config.preset'
 
 export default {
     presets: [preset],
     content: [
-        './app/Filament/Admin/**/*.php',
+        './app/Filament/**/*.php',
         './resources/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
     ],
