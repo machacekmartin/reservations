2dc5f930854130f70e00aa065bc6b1d7b083cc1f
Martin <macamp34@gmail.com>
2023-09-06 18:13:40 +0200

    Fix phpstan error

--- app/Livewire/TableInner.php
+++ app/Livewire/TableInner.php
@@ -3,13 +3,14 @@
 namespace App\Livewire;
 
 use App\Models\Table;
+use Illuminate\View\View;
 use Livewire\Component;
 
 class TableInner extends Component
 {
     public Table $table;
 
-    public function render()
+    public function render(): View
     {
         return view('livewire.table-inner');
     }
