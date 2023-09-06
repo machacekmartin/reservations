08ed894b663a038fe8ae0cf97cf901ef6858d836
Martin <macamp34@gmail.com>
2023-09-06 18:13:12 +0200

    Modify table list page to use custom blade

--- app/Filament/Admin/Resources/TableResource/Pages/ListTables.php
+++ app/Filament/Admin/Resources/TableResource/Pages/ListTables.php
@@ -3,17 +3,19 @@
 namespace App\Filament\Admin\Resources\TableResource\Pages;
 
 use App\Filament\Admin\Resources\TableResource;
-use Filament\Actions;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListTables extends ListRecords
 {
     protected static string $resource = TableResource::class;
 
+    protected static string $view = 'filament.admin.resources.table-resource.pages.list-tables';
+
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            CreateAction::make(),
         ];
     }
 }
---
+++ resources/views/filament/admin/resources/table-resource/pages/list-tables.blade.php
@@ -0,0 +1,7 @@
+<x-filament-panels::page>
+    {{ $this->table }}
+
+    <x-interactive-canvas>
+        <livewire:edit-interactive-tables />
+    </x-interactive-canvas>
+</x-filament-panels::page>
