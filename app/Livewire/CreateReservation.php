8d6f39fc72044514c3be22322409a8719fc6a219
Martin <macamp34@gmail.com>
2023-09-08 01:46:45 +0200

    Fully implement create form of reservation

--- app/Livewire/CreateReservation.php
+++ app/Livewire/CreateReservation.php
@@ -4,33 +4,45 @@ namespace App\Livewire;
 
 use App\Models\Table;
 use Carbon\CarbonInterval;
+use Closure;
+use Filament\Forms\Components\Actions;
+use Filament\Forms\Components\Actions\Action;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
+use Filament\Notifications\Notification;
 use Illuminate\Support\Carbon;
-use Illuminate\View\View;
-use Livewire\Attributes\Computed;
+use Illuminate\Support\Collection;
 use Livewire\Attributes\On;
 use Livewire\Component;
 
+/**
+ * @property-read Form $form
+ */
 class CreateReservation extends Component implements HasForms
 {
     use InteractsWithForms;
-
-    public string $date;
-    public string $count;
 
     /**
      * @var array<int>
      */
     public array $selectedTables = [];
 
-    public function render(): View
+    /**
+     * @var array<string, mixed>
+     */
+    public ?array $data = [];
+
+    public function mount(): void
     {
-        return view('livewire.create-reservation');
+        $this->form->fill();
     }
 
     /**
@@ -40,6 +52,9 @@ class CreateReservation extends Component implements HasForms
     public function updateSelectedTables(array $tables): void
     {
         $this->selectedTables = $tables;
+
+        $this->data['start_at'] = null;
+        $this->data['end_at'] = null;
     }
 
     public function form(Form $form): Form
@@ -47,43 +62,140 @@ class CreateReservation extends Component implements HasForms
         return $form
             ->schema([
                 Section::make()
-                    ->columns(1)
+                    ->columns(3)
                     ->schema([
                         DatePicker::make('date')
-                            ->label('What day?')
+                            ->label('Day of reservation')
                             ->required()
-                            ->prefixIcon('heroicon-o-calendar'),
+                            ->reactive()
+                            ->minDate(now()->startOfDay())
+                            ->afterStateUpdated(fn (Set $set) => $set('start_at', null))
+                            ->prefixIcon('heroicon-o-calendar')
+                            ->dehydrated(false),
 
-                        TextInput::make('count')
-                            ->label('How many people')
-                            ->numeric()
-                            ->minValue(1)
+                        Select::make('start_at')
+                            ->label('Start of reservation')
                             ->required()
-                            ->prefixIcon('heroicon-o-user'),
+                            ->options(fn () => $this->timeOptions())
+                            ->disableOptionWhen(fn (string $value) => $this->isStartOptionDisabled($value))
+                            ->disabled(fn (Get $get) => $get('date') == null || empty($this->selectedTables))
+                            ->afterStateUpdated(fn (Set $set) => $set('end_at', null))
+                            ->reactive()
+                            ->prefixIcon('heroicon-o-clock')
+                            ->rules([fn () => $this->getStartAtRule()]),
+
+                        Select::make('end_at')
+                            ->label('End of reservation')
+                            ->required()
+                            ->options(fn () => $this->timeOptions())
+                            ->disableOptionWhen(fn (string $value, Get $get) => $this->isEndOptionDisabled($get('start_at'), $value))
+                            ->disabled(fn (Get $get) => $get('start_at') == null)
+                            ->reactive()
+                            ->prefixIcon('heroicon-o-clock')
+                            ->rules([fn (Get $get) => $this->getEndAtRule($get)])
                     ]),
-            ]);
+                    Section::make()
+                        ->columns(2)
+                        ->schema([
+                            Select::make('remind_at')
+                                ->label('Remind me at')
+                                ->default(0)
+                                ->options([
+                                    0 => "Don't remind me",
+                                    15 => '15 minutes before reservation',
+                                    30 => '30 minutes before reservation',
+                                    60 => '1 hour before reservation',
+                                    120 => '2 hours before reservation',
+                                ])
+                                ->in([0, 15, 30, 60, 120])
+                                ->dehydrateStateUsing(fn (int $state, Get $get) =>
+                                    Carbon::parse($get('start_at'))->addMinutes($state)->toDateTimeString()
+                                ),
+
+                            TextInput::make('guest_count')
+                                ->label('Hwo many people will be coming')
+                                ->required()
+                                ->numeric()
+                                ->minValue(1)
+                                ->default(1)
+                                ->maxValue(20)
+                                ->prefixIcon('heroicon-o-user'),
+
+                            Textarea::make('note')
+                                ->label('Note')
+                                ->placeholder('Anything else we should know?')
+                                ->rows(3)
+                                ->columnSpanFull()
+                                ->maxLength(50)
+                    ]),
+                    Actions::make([
+                        Action::make('save')
+                            ->label('Create reservation')
+                            ->size('xl')
+                            ->icon('heroicon-o-check')
+                            ->action(fn () => $this->submit())
+                    ])
+            ])->statePath('data');
     }
 
-    /**
-     * @return array<string, mixed>
-     */
-    #[Computed]
-    public function times(): array
+    private function submit(): void
     {
-        $intervals = CarbonInterval::minutes(20)->toPeriod('10:00', '20:00')->toArray();
+        $tables = $this->selectedTables;
 
-        return collect($intervals)
-            ->map(function ($interval) {
-                $start = $interval->toDate();
+        /** @var \App\Models\User $user */
+        $user = auth()->user();
 
-                $reserved = collect($this->selectedTables)
-                    ->contains(fn ($tableId) => Table::query()->findOrFail($tableId)->isReservedAt(Carbon::instance($start))
-                    );
+        $reservation = $user->reservations()->create(
+            $this->form->getState(),
+        );
 
-                return [
-                    'time' => $start,
-                    'reserved' => $reserved,
-                ];
-            })->toArray();
+        $reservation->tables()->attach($tables);
+
+        Notification::make()->success()->title('Reservation for '. $reservation->start_at->format('H:i') . ' created')->send();
+        redirect()->route('reservations');
+    }
+
+    private function isStartOptionDisabled(string $start): bool
+    {
+        return collect($this->selectedTables)
+            ->contains(function ($tableId) use ($start) {
+                return Table::query()->findOrFail($tableId)->isReservedAt(Carbon::parse($start));
+            });
+    }
+
+    private function isEndOptionDisabled(?string $start, string $end): bool
+    {
+        if ($start >= $end) return true;
+
+        return collect($this->selectedTables)
+            ->contains(function ($tableId) use ($start, $end) {
+                return Table::query()->findOrFail($tableId)->isReservedBetween(Carbon::parse($start), Carbon::parse($end));
+            });
+    }
+
+    private function timeOptions(): Collection
+    {
+        $intervals = CarbonInterval::minutes(20)->toPeriod(
+            $this->data['date'] . ' 10:00',  /** @phpstan-ignore-line */
+            $this->data['date'] . ' 20:00'   /** @phpstan-ignore-line */
+        )->toArray();
+
+        return collect($intervals)->mapWithKeys(fn ($item) =>
+            [$item->toDateTimeString() => $item->format('H:i')]
+        );
+    }
+
+    private function getEndAtRule(Get $get): Closure
+    {
+        return function ($attribute, $value, Closure $fail) use ($get) {
+            if ($this->isEndOptionDisabled($get('start_at'), $value)) $fail('The :attribute is invalid.');
+        };
+    }
+
+    private function getStartAtRule(): Closure
+    {
+        return function ($attribute, $value, Closure $fail) {
+            if ($this->isStartOptionDisabled($value)) $fail('The :attribute is invalid.');
+        };
     }
 }
--- resources/views/livewire/create-reservation.blade.php
+++ resources/views/livewire/create-reservation.blade.php
@@ -1,25 +1,11 @@
 <div>
-    <div class="mb-6">
-        <h2 class="mb-2 font-semibold">1. Which day</h2>
-        <form>
-            {{ $this->form }}
-        </form>
-    </div>
-
-    <h2 class="mb-2 font-semibold">2. Select available tables</h2>
+    <h2 class="mb-2 font-semibold">1. Select from available tables</h2>
     <x-interactive-canvas>
         <livewire:select-interactive-tables />
     </x-interactive-canvas>
 
-    <div>
-        <h2 class="mb-2 font-semibold">3. Time range</h2>
-        <ul>
-            @foreach ($this->times as $time)
-                <li>
-                    {{ $time['time']->format('H:i') }}
-                    {{ $time['reserved'] ? 'reserved' : 'available' }}
-                </li>
-            @endforeach
-        </ul>
+    <div class="mt-6">
+        <h2 class="mb-2 font-semibold">2. When?</h2>
+            {{ $this->form }}
     </div>
 </div>
