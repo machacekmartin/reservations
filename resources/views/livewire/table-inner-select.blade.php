846f9eecfb555b3f9c1bd6d0772fe35de032c975
Martin <macamp34@gmail.com>
2023-09-08 01:46:25 +0200

    Change styles for inner table selector component

--- resources/views/livewire/table-inner-select.blade.php
+++ resources/views/livewire/table-inner-select.blade.php
@@ -6,10 +6,10 @@
 <div class="relative">
     <div
         @class([
-            'p-2 text-white transition-transform shadow-2xl rounded-md ring ring-white/20 flex flex-col',
-            'bg-gradient-to-tr from-gray-200/60 to-gray-400 cursor-default' => $permanentlyUnavailable,
-            'bg-gradient-to-tr from-sky-400/60 to-sky-500/60 cursor-pointer hover:bg-green-400/20' => ! $permanentlyUnavailable && ! $selected,
-            'bg-gradient-to-tr from-green-400/60 to-green-500/60' => ! $permanentlyUnavailable && $selected
+            'p-2 text-white transition-transform rounded-md ring ring-white/20 flex flex-col transition-all',
+            'bg-gradient-to-tr from-gray-200/60 to-gray-400 cursor-default ' => $permanentlyUnavailable,
+            'bg-sky-500/60 cursor-pointer hover:bg-sky-500/80 shadow-2xl' => ! $permanentlyUnavailable && ! $selected,
+            'bg-gradient-to-tr from-emerald-400/80 to-sky-500/80 bg-sky-500/60 shadow-2xl' => ! $permanentlyUnavailable && $selected
         ])
         style="width: {{ $table->dimensions->width }}px; height: {{ $table->dimensions->height }}px;"
     >
@@ -22,15 +22,15 @@
                 <span class="text-sm font-bold uppercase">
                     {{ $table->label }}
                 </span>
-                <span class="inline-flex items-center text-sm gap-x-1">
-                    <x-filament::icon class="w-4 h-4 text-white" icon="heroicon-o-user" />
+                <span class="inline-flex items-center text-md gap-x-1">
+                    <x-filament::icon class="w-5 h-5 text-white" icon="heroicon-o-user" />
                     {{ $table->capacity }}
                 </span>
             </div>
             <ul class="mt-auto">
                 <li class="pt-1 text-sm">
                     @if ($selected)
-                        <x-filament::icon icon="heroicon-o-check-circle" class="w-4 h-4" />
+                        <x-filament::icon icon="heroicon-o-check-circle" class="w-6 h-6" />
                     @endif
                 </li>
             </ul>
