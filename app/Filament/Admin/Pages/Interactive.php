4318a44e3ac73aa2ae7ca531be92bc13b4718b90
Martin <macamp34@gmail.com>
2023-09-08 06:00:03 +0200

    Change height + remove title

--- app/Filament/Admin/Pages/Interactive.php
+++ app/Filament/Admin/Pages/Interactive.php
@@ -3,10 +3,16 @@
 namespace App\Filament\Admin\Pages;
 
 use Filament\Pages\Page;
+use Illuminate\Contracts\Support\Htmlable;
 
 class Interactive extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.admin.pages.interactive';
+
+    public function getTitle(): string|Htmlable
+    {
+        return '';
+    }
 }
--- resources/views/components/interactive-canvas.blade.php
+++ resources/views/components/interactive-canvas.blade.php
@@ -3,7 +3,6 @@
     start: { x: 0, y: 0 },
     current: { x: 0, y: 0 },
     press(x, y) {
-        {{-- document.documentElement.style.overflow = 'hidden' --}}
         this.dragging = true;
         this.start.x = x - $refs.scrollable.offsetLeft;
         this.start.y = y - $refs.scrollable.offsetTop;
@@ -20,12 +19,11 @@
         $refs.scrollable.scrollTop = this.current.y - differenceY;
     },
     pause() {
-        {{-- document.documentElement.style.overflow = 'auto' --}}
         this.dragging = false;
     }
 }">
     <div
-        class="relative  border border-gray-200 shadow rounded-2xl bg-slate-800 cursor-grab active:cursor-grabbing overflow-hidden h-[550px] min-w-[200px]"
+        class="relative  border border-gray-200 shadow rounded-2xl bg-slate-800 cursor-grab active:cursor-grabbing overflow-hidden h-[650px] min-w-[200px]"
         x-ref="scrollable"
         x-on:mousedown.self="event => press(event.clientX, event.clientY)"
         x-on:touchstart.self.passive="event => press(event.touches[0].clientX, event.touches[0].clientY)"
